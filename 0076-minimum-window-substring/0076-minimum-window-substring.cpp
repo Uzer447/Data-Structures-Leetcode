694c1ea4212816c1199e3f5ac793d8f5d3b7830a
Uzer447 <103814661+Uzer447@users.noreply.github.com>
2025-01-24 15:54:25 +0530

    Time: 8 ms (61.87%), Space: 11.5 MB (60.7%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -6,16 +6,16 @@ public:
         for (int i = 0; i < t.size(); i++) {
             m[t[i]]++;
         }
-        
+
         int left = 0;
         int right = 0;
-        
+
         // # of chars in t that must be in s
         int counter = t.size();
-        
+
         int minStart = 0;
         int minLength = INT_MAX;
-        
+
         while (right < s.size()) {
             // if char in s exists in t, decrease
             if (m[s[right]] > 0) {
@@ -25,14 +25,14 @@ public:
             m[s[right]]--;
             // move right to find valid window
             right++;
-            
+
             // when window found, move left to find smaller
             while (counter == 0) {
                 if (right - left < minLength) {
                     minStart = left;
                     minLength = right - left;
                 }
-                
+
                 m[s[left]]++;
                 // when char exists in t, increase
                 if (m[s[left]] > 0) {
@@ -41,7 +41,7 @@ public:
                 left++;
             }
         }
-        
+
         if (minLength != INT_MAX) {
             return s.substr(minStart, minLength);
         }
