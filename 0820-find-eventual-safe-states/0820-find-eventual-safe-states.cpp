baf0ba4a9684fc449799739be87f3906785fb739
Uzer447 <103814661+Uzer447@users.noreply.github.com>
2025-01-24 16:22:14 +0530

    Time: 24 ms (65.24%), Space: 59.6 MB (62.24%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -2,13 +2,16 @@ class Solution {
     vector<int> color;
     vector<vector<int>> g;
     bool dfs(int u) {
-        if (color[u]) return color[u] == 2;
+        if (color[u])
+            return color[u] == 2;
         color[u] = 1;
         for (int v : g[u])
-            if (!dfs(v)) return false;
+            if (!dfs(v))
+                return false;
         color[u] = 2;
         return true;
     }
+
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         g = graph;
@@ -16,7 +19,8 @@ public:
         color.resize(n);
         vector<int> ans;
         for (int i = 0; i < n; ++i)
-            if (dfs(i)) ans.push_back(i);
+            if (dfs(i))
+                ans.push_back(i);
         return ans;
     }
 };
