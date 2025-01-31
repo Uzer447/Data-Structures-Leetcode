77492e8184ebfcf78abf423bdcf2a816bedeeb48
Uzer447 <103814661+Uzer447@users.noreply.github.com>
2025-01-31 15:51:29 +0530

    Time: 15 ms (40.49%) | Memory: 99.3 MB (15.38%) - LeetSync

--- 2904-sorting-three-groups/sorting-three-groups.cpp
+++ 2904-sorting-three-groups/sorting-three-groups.cpp
@@ -1,30 +1,13 @@
 class Solution {
 public:
-    int minOps(int i, int j, vector<int>& nums) {
-        int count = 0;
-        for(int x=0;x<i;x++) {
-            if(nums[x] != 1) count++;
-        // if the value at x is not 1, an operation is needed!
-        }
-        for(int x=i;x<j;x++) {
-            if(nums[x] != 2) count++;
-        // if the value at x is not 2, an operation is needed!
-        }
-        for(int x=j;x<nums.size();x++) {
-            if(nums[x] != 3) count++;
-        // if the value at x is not 3, an operation is needed!
-        }
-        return count;
-    }
     int minimumOperations(vector<int>& nums) {
-        int out = INT_MAX;
         int n = nums.size();
-        for(int i=0;i<=n;i++) {
-            for(int j=i;j<=n;j++) {
-                out = min(out, minOps(i, j, nums));
-            }
+        vector<vector<int>> dp(n + 1, vector<int>(3, 0));
+        for (int i = 1; i <= n; i++) {
+            dp[i][0] = (nums[i - 1] != 1) + dp[i - 1][0];
+            dp[i][1] = (nums[i - 1] != 2) + min(dp[i - 1][0], dp[i - 1][1]);
+            dp[i][2] = (nums[i - 1] != 3) + min({dp[i - 1][0], dp[i - 1][1], dp[i - 1][2]});
         }
-        return out;
+        return min({dp[n][0], dp[n][1], dp[n][2]});
     }
 };
-// Please consider upvoting if the solution helped! Thank you :)
