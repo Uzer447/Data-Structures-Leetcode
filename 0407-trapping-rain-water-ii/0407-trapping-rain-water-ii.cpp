c0d61169b3a1740843081a1e486a703b3d808a32
Uzer447 <103814661+Uzer447@users.noreply.github.com>
2025-02-08 01:32:37 +0530

    Time: 30 ms (50.59%), Space: 18.7 MB (28.43%) - LeetHub

--- 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.cpp
+++ 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.cpp
@@ -1,40 +1,36 @@
-#include <vector>
-#include <queue>
-#include <tuple>
-using namespace std;
-
 class Solution {
 public:
     int trapRainWater(vector<vector<int>>& heightMap) {
-        if(heightMap.empty() || heightMap[0].empty())
-            return 0;
-        int m = heightMap.size(), n = heightMap[0].size();
+        int m = heightMap.size();
+        if (m <= 2) return 0;
+        int n = heightMap[0].size();
+        if (n <= 2) return 0;
         vector<vector<bool>> visited(m, vector<bool>(n, false));
-        typedef tuple<int, int, int> T;
-        priority_queue<T, vector<T>, greater<T>> pq;
-        for(int i = 0; i < m; ++i) {
-            pq.push({heightMap[i][0], i, 0});
-            visited[i][0] = true;
-            pq.push({heightMap[i][n-1], i, n-1});
-            visited[i][n-1] = true;
+        struct Cell {
+            int h, x, y;
+            Cell(int height, int i, int j) : h(height), x(i), y(j) {}
+        };
+        auto cmp = [](const Cell& a, const Cell& b) { return a.h > b.h; };
+        priority_queue<Cell, vector<Cell>, decltype(cmp)> pq(cmp);
+        for (int i = 0; i < m; ++i) {
+            pq.emplace(heightMap[i][0], i, 0);
+            pq.emplace(heightMap[i][n - 1], i, n - 1);
+            visited[i][0] = visited[i][n - 1] = true;
         }
-        for(int j = 1; j < n-1; ++j) {
-            pq.push({heightMap[0][j], 0, j});
-            visited[0][j] = true;
-            pq.push({heightMap[m-1][j], m-1, j});
-            visited[m-1][j] = true;
+        for (int j = 1; j < n - 1; ++j) {
+            pq.emplace(heightMap[0][j], 0, j);
+            pq.emplace(heightMap[m - 1][j], m - 1, j);
+            visited[0][j] = visited[m - 1][j] = true;
         }
-        int res = 0;
-        int dirs[4][2] = {{-1,0},{1,0},{0,-1},{0,1}};
-        while(!pq.empty()) {
-            auto [height, x, y] = pq.top();
-            pq.pop();
-            for(auto& dir : dirs) {
-                int nx = x + dir[0], ny = y + dir[1];
-                if(nx >= 0 && nx < m && ny >= 0 && ny < n && !visited[nx][ny]) {
-                    visited[nx][ny] = true;
-                    res += max(0, height - heightMap[nx][ny]);
-                    pq.push({max(heightMap[nx][ny], height), nx, ny});
+        int res = 0, dirs[4][2] = {{-1,0},{1,0},{0,-1},{0,1}};
+        while (!pq.empty()) {
+            auto cell = pq.top(); pq.pop();
+            for (auto& d : dirs) {
+                int x = cell.x + d[0], y = cell.y + d[1];
+                if (x >= 0 && x < m && y >= 0 && y < n && !visited[x][y]) {
+                    visited[x][y] = true;
+                    res += max(0, cell.h - heightMap[x][y]);
+                    pq.emplace(max(heightMap[x][y], cell.h), x, y);
                 }
             }
         }
