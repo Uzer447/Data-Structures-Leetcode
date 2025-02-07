45264ca6f8f700cd33a4bf0bcbddce2a9e363e1f
Uzer447 <103814661+Uzer447@users.noreply.github.com>
2025-02-07 14:03:20 +0530

    Time: 0 ms (100%), Space: 9.5 MB (31.19%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -2,24 +2,26 @@ class Solution {
 public:
     bool isMatch(string s, string p) {
         int m = s.size(), n = p.size();
-        vector<vector<bool>> dp(m+1, vector<bool>(n+1, false));
-        dp[0][0] = true;
-        for(int j=2; j<=n; j++)
-            if(p[j-1]=='*')
-                dp[0][j]=dp[0][j-2];
-        for(int i=1; i<=m; i++) {
-            for(int j=1; j<=n; j++) {
-                if(p[j-1]==s[i-1] || p[j-1]=='.')
-                    dp[i][j]=dp[i-1][j-1];
-                else if(p[j-1]=='*') {
-                    dp[i][j]=dp[i][j-2];
-                    if(p[j-2]==s[i-1] || p[j-2]=='.')
-                        dp[i][j]=dp[i][j]|dp[i-1][j];
-                }
-                else
-                    dp[i][j]=false;
+        vector<vector<int>> memo(m + 1, vector<int>(n + 1, -1));
+        return dp(0, 0, s, p, memo);
+    }
+    bool dp(int i, int j, string& s, string& p, vector<vector<int>>& memo) {
+        if (memo[i][j] != -1)
+            return memo[i][j];
+        int m = s.size(), n = p.size();
+        bool ans;
+        if (j == n) {
+            ans = (i == m);
+        } else {
+            bool first_match = (i < m && (p[j] == s[i] || p[j] == '.'));
+            if (j + 1 < n && p[j + 1] == '*') {
+                ans = dp(i, j + 2, s, p, memo) ||
+                      (first_match && dp(i + 1, j, s, p, memo));
+            } else {
+                ans = first_match && dp(i + 1, j + 1, s, p, memo);
             }
         }
-        return dp[m][n];
+        memo[i][j] = ans;
+        return ans;
     }
 };
