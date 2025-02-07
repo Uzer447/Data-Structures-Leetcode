fbe9cd0bc352f798516ff6aa7ff07cff7c0bbe35
Uzer447 <103814661+Uzer447@users.noreply.github.com>
2025-02-07 14:29:37 +0530

    Time: 39 ms (39.28%), Space: 24.5 MB (13.4%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
@@ -1,36 +1,26 @@
 class Solution {
 public:
     int maxPoints(vector<vector<int>>& points) {
-        int n = points.size(), res = 0;
-        if (n <= 2) return n;
-        for (int i = 0; i < n; ++i) {
-            map<pair<int, int>, int> cnt;
-            int dup = 1, max_cnt = 0;
-            for (int j = i + 1; j < n; ++j) {
-                int dx = points[j][0] - points[i][0], dy = points[j][1] - points[i][1];
-                if (dx == 0 && dy == 0) {
-                    ++dup;
-                } else {
-                    int g = gcd(dx, dy);
-                    dx /= g;
-                    dy /= g;
-                    if (dx < 0) {
-                        dx = -dx;
-                        dy = -dy;
-                    }
-                    if (dx == 0 && dy < 0) {
-                        dy = -dy;
-                    }
-                    ++cnt[{dy, dx}];
-                    max_cnt = max(max_cnt, cnt[{dy, dx}]);
-                }
+
+        int result = INT_MIN;
+        int n = points.size();
+        if (n == 1)
+            return 1;
+        int slope;
+       
+        for (int i = 0; i < n; i++) {
+             unordered_map<double,int>mp ; 
+            for (int j =0; j < n; j++) {
+                if(i==j)continue;
+                int dy = (points[j][1] - points[i][1]);
+                int dx = (points[j][0] - points[i][0]);
+               double theta = atan2(dy,dx) ; 
+               mp[theta]++ ;  
             }
-            res = max(res, max_cnt + dup);
+            for(auto it :mp){
+                result =max(result , it.second+1) ; 
+            }
         }
-        return res;
-    }
-private:
-    int gcd(int a, int b) {
-        return b == 0 ? a : gcd(b, a % b);
+        return result;
     }
 };
