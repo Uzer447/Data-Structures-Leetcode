65b010826482970046d9053439dc2cffbb9ffcda
Uzer447 <103814661+Uzer447@users.noreply.github.com>
2025-02-08 01:14:47 +0530

    Time: 0 ms (100%), Space: 30.1 MB (55.15%) - LeetHub

--- 0335-self-crossing/0335-self-crossing.cpp
+++ 0335-self-crossing/0335-self-crossing.cpp
@@ -1,10 +1,15 @@
 class Solution {
 public:
-    bool isSelfCrossing(vector<int>& x) {
-        for(int i = 3; i < x.size(); ++i) {
-            if(x[i] >= x[i-2] && x[i-1] <= x[i-3]) return true;
-            if(i >= 4 && x[i-1] == x[i-3] && x[i] + x[i-4] >= x[i-2]) return true;
-            if(i >= 5 && x[i-2] >= x[i-4] && x[i] + x[i-4] >= x[i-2] && x[i-1] >= x[i-3] - x[i-5] && x[i-1] <= x[i-3] && x[i-5] <= x[i-3]) return true;
+    bool isSelfCrossing(vector<int>& d) {
+        int n = d.size();
+        for (int i = 3; i < n; ++i) {
+            if (d[i] >= d[i - 2] && d[i - 1] <= d[i - 3])
+                return true;
+            if (i >= 4 && d[i - 1] == d[i - 3] && d[i] + d[i - 4] >= d[i - 2])
+                return true;
+            if (i >= 5 && d[i - 2] >= d[i - 4] && d[i] + d[i - 4] >= d[i - 2] &&
+                d[i - 1] <= d[i - 3] && d[i - 1] + d[i - 5] >= d[i - 3])
+                return true;
         }
         return false;
     }
