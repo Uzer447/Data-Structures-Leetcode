218674f7e7310bef48f35bc376922282424dd4a1
Uzer447 <103814661+Uzer447@users.noreply.github.com>
2025-02-16 14:23:15 +0530

    Time: 2 ms (19.67%), Space: 17.5 MB (28.17%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -1,47 +1,40 @@
 class Solution {
 public:
-    bool dfs(int node,int col,vector<vector<int>> &adj,vector<int> &color)
+    bool check(int src,vector<vector<int>> &graph,vector<char> &vis)
     {
-        color[node]=col;
-        for(auto it:adj[node])
+        queue<pair<int,char>> q;
+        q.push({src,'A'});
+        vis[src]='A';
+        while(!q.empty())
         {
-            if(color[it]==-1)
-            {
-                if(dfs(it,1-col,adj,color)==false)
-                return false;
-            }
-            else if(color[it]==color[node])
-            {
-                return false;
-            }
-        }
-        return true;
-    }
-    bool check(int src,vector<vector<int>>& adj, vector<int>& color) {
-        queue<int> q;
-        color[src] = 0;
-        q.push(src);
-        while (!q.empty()) {
-            int node = q.front();
+            int node=q.front().first;
+            int color=q.front().second;
             q.pop();
-            for (auto it : adj[node]) {
-                if (color[it] == -1) {
-                    q.push(it);
-                    color[it] = 1 - color[node];
-                } else {
-                    if (color[it] == color[node])
-                        return false;
+            for(auto it:graph[node])
+            {
+                if(vis[it]=='#')
+                {
+                    q.push({it,(color=='A'?'B':'A')});
+                    vis[it]=(color=='A'?'B':'A');
+                }
+                else 
+                {
+                    if(vis[it]==color)
+                    return false;
                 }
             }
         }
         return true;
     }
     bool isBipartite(vector<vector<int>>& graph) {
-        vector<int> color(graph.size(), -1);
-        for (int i = 0; i < graph.size(); i++) {
-            if (color[i] == -1) {
-            if(dfs(i,0,graph,color)==false)
-            return false;
+        int n=graph.size();
+        vector<char> vis(n,'#');
+        for(int i=0;i<n;i++)
+        {
+            if(vis[i]=='#')
+            {
+                if(check(i,graph,vis)==false)
+                return false;
             }
         }
         return true;
