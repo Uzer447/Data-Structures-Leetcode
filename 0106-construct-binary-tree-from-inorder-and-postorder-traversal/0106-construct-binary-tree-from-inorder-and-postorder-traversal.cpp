1efa0739fd4a0830332750f7bf298f3b63f88455
Uzer447 <103814661+Uzer447@users.noreply.github.com>
2025-02-18 14:31:54 +0530

    Time: 4 ms (51.83%), Space: 27.5 MB (58.42%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -6,29 +6,34 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        map<int,int> hm;
-        for(int i=0;i<inorder.size();i++)
-        hm[inorder[i]]=i;
-
-        return buildTree(inorder,0,inorder.size()-1,postorder,0,postorder.size()-1,hm);
+        map<int, int> mp;
+        for (int i = 0; i < inorder.size(); i++) {
+            mp[inorder[i]] = i;
+        }
+        TreeNode* root = build(postorder, 0, postorder.size() - 1, inorder, 0,
+                               inorder.size() - 1, mp);
+        return root;
     }
-
-    TreeNode* buildTree(vector<int> &inorder,int is,int ie,vector<int> &postorder,int ps,int pe,map<int,int> &hm)
-    {
-        if(ps>pe || is>ie) return NULL;
-        TreeNode* root=new TreeNode(postorder[pe]);
-        int inRoot=hm[postorder[pe]];
-        int numsleft=inRoot-is;
-
-        root->left=buildTree(inorder,is,inRoot-1,postorder,ps,ps+numsleft-1,hm);
-        root->right=buildTree(inorder,inRoot+1,ie,postorder,ps+numsleft,pe-1,hm);
-
+    TreeNode* build(vector<int>& postorder, int poststart, int postend,
+                    vector<int>& inorder, int instart, int inend,
+                    map<int, int>& mp) {
+        if (poststart > postend || instart > inend) {
+            return NULL;
+        }
+        TreeNode* root = new TreeNode(postorder[postend]);
+        int inroot = mp[root->val];
+        int numsleft = inroot - instart;
+        root->left = build(postorder, poststart, poststart + numsleft - 1,
+                           inorder, instart, inroot - 1, mp);
+        root->right = build(postorder, poststart + numsleft, postend - 1,
+                            inorder, inroot + 1, inend, mp);
         return root;
     }
 };
