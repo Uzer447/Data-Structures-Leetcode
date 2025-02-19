054be03e300a10756310ce808b6c5c9a368eb6f0
Uzer447 <103814661+Uzer447@users.noreply.github.com>
2025-02-19 20:11:36 +0530

    Time: 3 ms (53.08%), Space: 17.7 MB (98.51%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
-    static bool compareByFreq(const pair<int, int>& a,
-                              const pair<int, int>& b) {
-        return a.second > b.second;
-    }
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int, int> mp;
-        for (int i = 0; i < nums.size(); i++) {
-            mp[nums[i]]++;
+        if(k==nums.size())
+        return nums;
+        unordered_map<int,int> mp;
+        for(auto it:nums)
+        {
+            mp[it]++;
+        }   
+        auto comp = [&mp](int n1, int n2) { return mp[n1] > mp[n2]; };
+        priority_queue<int,vector<int>,decltype(comp)> pq(comp);
+        for(auto it:mp)
+        {
+            pq.push(it.first);
+            if(pq.size()>k) pq.pop();
         }
-        vector<pair<int, int>> freq(mp.begin(), mp.end());
-        sort(freq.begin(), freq.end(), compareByFreq);
+        vector<int> ans(k);
+        for(int i=0;i<k;i++)
+        {
+            ans[k-1-i]=pq.top();
+            pq.pop();
+        }
+        return ans;
 
-        vector<int> result;
-        for (const auto& i : freq) {
-            if (k > 0) {
-                result.push_back(i.first);
-                k--;
-            }
-        }
-        return result;
     }
 };
