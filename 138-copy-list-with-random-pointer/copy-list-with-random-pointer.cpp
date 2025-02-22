06bec9ac9398759b8672cb1c7edfa55df06aada1
Uzer447 <103814661+Uzer447@users.noreply.github.com>
2025-02-22 17:27:21 +0530

    Time: 0 ms (100.00%) | Memory: 14.8 MB (99.65%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -13,11 +13,48 @@ public:
     }
 };
 */
-
+void insertCopyInBetween(Node* head){
+    Node* temp = head;
+    while(temp != NULL){
+        Node* nextElement = temp->next;
+        Node* copy = new Node(temp->val);  
+        copy->next = nextElement;  
+        temp->next = copy;         
+        temp = nextElement;         
+    }
+}
+void connectRandomPointers(Node* head){
+    Node* temp = head;
+    while(temp != NULL){
+        Node* copyNode = temp->next;    
+        if(temp->random){   
+            copyNode->random = temp->random->next; 
+        }
+        else{
+            copyNode->random = NULL;   
+        }
+        temp = temp->next->next;   
+    }
+}
+Node* getDeepCopyList(Node* head){
+    Node* temp = head;
+    Node* dummyNode = new Node(-1);   
+    Node* res = dummyNode;             
+    while(temp != NULL){
+        res->next = temp->next;
+        res = res->next;
+        temp->next = temp->next->next;
+        temp = temp->next;
+    }
+    return dummyNode->next;   
+}
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
         if(head==NULL) return NULL;
+        insertCopyInBetween(head);
+        connectRandomPointers(head);
+        return getDeepCopyList(head);
         Node* newhead=new Node(head->val);
         Node* temp2=newhead;
         map<Node*,Node*> mp;
