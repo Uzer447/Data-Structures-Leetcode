093e515adc7dd9253dba6dd903adcc1fdacc5d81
Uzer447 <103814661+Uzer447@users.noreply.github.com>
2025-02-24 19:29:04 +0530

    Time: 563 ms (15.03%) | Memory: 296.7 MB (6.62%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector < vector<int>> dp(n + 1, vector<int>(n + 1, 0));
-        for (int ind = n - 1; ind >= 0; ind--) {
-            for (int prevind = ind-1; prevind >= -1; prevind--) {
-                int notTake = 0 + dp[ind + 1][prevind+1];
-                int take = INT_MIN;
-                if (prevind == -1 || nums[ind] > nums[prevind]) {
-                    take = 1 + dp[ind + 1][ind+1];
-                }
-                dp[ind][prevind+1] = max(take, notTake);
-            }
+    int func(int ind,int prevind,vector<int> &nums,vector<vector<int>> &dp)
+    {
+        if(ind==nums.size())
+        {
+            return 0;
         }
-        return dp[0][0];
+        if(dp[ind][prevind+1]!=-1)
+        return dp[ind][prevind+1];
+        int take=0;
+        int nottake=func(ind+1,prevind,nums,dp);
+        if(prevind==-1 || nums[ind]>nums[prevind])
+        {
+            take=1+func(ind+1,ind,nums,dp);
+        }
+        return dp[ind][prevind+1]=max(take,nottake);
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        int n=nums.size();
+        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        return func(0,-1,nums,dp);
     }
 };
