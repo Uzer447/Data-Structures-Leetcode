19d1b3ab8b07b85b9a9abd2689e5d0ca92fcde0d
Uzer447 <103814661+Uzer447@users.noreply.github.com>
2025-03-25 15:18:55 +0530

    Time: 263 ms (47.27%), Space: 226.7 MB (51.64%) - LeetHub

--- 3657-check-if-grid-can-be-cut-into-sections/3657-check-if-grid-can-be-cut-into-sections.cpp
+++ 3657-check-if-grid-can-be-cut-into-sections/3657-check-if-grid-can-be-cut-into-sections.cpp
@@ -1,80 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-struct Candidate {
-    int pos, lower, upper;
-};
-
-bool solveDim(int n, int m, const vector<int>& starts,
-              const vector<int>& ends) {
-    vector<int> cand;
-    for (auto s : starts)
-        if (s > 0 && s < n)
-            cand.push_back(s);
-    for (auto e : ends)
-        if (e > 0 && e < n)
-            cand.push_back(e);
-    sort(cand.begin(), cand.end());
-    cand.erase(unique(cand.begin(), cand.end()), cand.end());
-    vector<Candidate> valid;
-    for (auto pos : cand) {
-        int cntS = (int)(lower_bound(starts.begin(), starts.end(), pos) -
-                         starts.begin());
-        int cntE =
-            (int)(upper_bound(ends.begin(), ends.end(), pos) - ends.begin());
-        if (cntS != cntE)
-            continue;
-        Candidate c;
-        c.pos = pos;
-        c.lower = cntE;
-        c.upper = m - (int)(lower_bound(starts.begin(), starts.end(), pos) -
-                            starts.begin());
-        valid.push_back(c);
-    }
-    int sz = valid.size();
-    if (sz < 2)
-        return false;
-    for (int i = 0; i < sz - 1; i++) {
-        if (valid[i].lower < 1)
-            continue;
-        int thresh = m - 1 - valid[i].lower;
-        if (thresh < 1)
-            continue;
-        int l = i + 1, r = sz - 1, ans = -1;
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            if (valid[mid].upper <= thresh) {
-                ans = mid;
-                r = mid - 1;
-            } else {
-                l = mid + 1;
-            }
-        }
-        if (ans != -1 && valid[ans].upper >= 1)
-            return true;
-    }
-    return false;
-}
-
+ 
 class Solution {
 public:
     bool checkValidCuts(int n, vector<vector<int>>& rectangles) {
         int m = rectangles.size();
-        vector<int> xStarts, xEnds, yStarts, yEnds;
-        for (auto& r : rectangles) {
-            xStarts.push_back(r[0]);
-            yStarts.push_back(r[1]);
-            xEnds.push_back(r[2]);
-            yEnds.push_back(r[3]);
+        vector<int> xs, xe, ys, ye;
+        for(auto &r : rectangles){
+            xs.push_back(r[0]); ys.push_back(r[1]);
+            xe.push_back(r[2]); ye.push_back(r[3]);
         }
-        sort(xStarts.begin(), xStarts.end());
-        sort(xEnds.begin(), xEnds.end());
-        sort(yStarts.begin(), yStarts.end());
-        sort(yEnds.begin(), yEnds.end());
-        if (solveDim(n, m, yStarts, yEnds))
-            return true;
-        if (solveDim(n, m, xStarts, xEnds))
-            return true;
+        sort(xs.begin(), xs.end());
+        sort(xe.begin(), xe.end());
+        sort(ys.begin(), ys.end());
+        sort(ye.begin(), ye.end());
+        auto solve = [&](int n, int m, const vector<int>& s, const vector<int>& e) -> bool {
+            vector<int> v;
+            for(auto x : s) if(x > 0 && x < n) v.push_back(x);
+            for(auto x : e) if(x > 0 && x < n) v.push_back(x);
+            sort(v.begin(), v.end());
+            v.erase(unique(v.begin(), v.end()), v.end());
+            vector<array<int, 3>> cand;
+            for(auto pos : v) {
+                int cntS = lower_bound(s.begin(), s.end(), pos) - s.begin();
+                int cntE = upper_bound(e.begin(), e.end(), pos) - e.begin();
+                if(cntS != cntE) continue;
+                int lower = cntE;
+                int upper = m - (lower_bound(s.begin(), s.end(), pos) - s.begin());
+                cand.push_back({pos, lower, upper});
+            }
+            int sz = cand.size();
+            for (int i = 0; i < sz; i++){
+                if(cand[i][1] < 1) continue;
+                int need = m - 1 - cand[i][1];
+                if(need < 1) continue;
+                int lo = i + 1, hi = sz - 1, idx = -1;
+                while(lo <= hi) {
+                    int mid = (lo + hi) / 2;
+                    if(cand[mid][2] <= need) { idx = mid; hi = mid - 1; }
+                    else lo = mid + 1;
+                }
+                if(idx != -1 && cand[idx][2] >= 1) return true;
+            }
+            return false;
+        };
+        if(solve(n, m, ys, ye)) return true;
+        if(solve(n, m, xs, xe)) return true;
         return false;
     }
 };
