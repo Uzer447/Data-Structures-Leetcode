8d9eef1ff59083ae6cded6b07651ecd2b9caab59
Uzer447 <103814661+Uzer447@users.noreply.github.com>
2025-04-08 23:30:02 +0530

    Time: 0 ms (100%), Space: 27.2 MB (93.86%) - LeetHub

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
@@ -2,16 +2,23 @@ class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
         int n = nums.size();
-        for (int k = 0; ; k++) {
-            int start = 3 * k;
-            if (start >= n) return k;
-            unordered_set<int> s;
-            bool ok = true;
-            for (int i = start; i < n; i++) {
-                if (s.count(nums[i])) { ok = false; break; }
-                s.insert(nums[i]);
-            }
-            if (ok) return k;
+        int freq[101] = {0};
+        for (int x : nums) freq[x]++;
+        int dup = 0;
+        for (int i = 1; i <= 100; i++){
+            if(freq[i] > 1) dup++;
         }
+        if(dup == 0) return 0;
+        int ops = 0, idx = 0;
+        while(idx < n){
+            for (int i = 0; i < 3 && idx < n; i++, idx++){
+                int x = nums[idx];
+                if(freq[x] == 2) dup--;
+                freq[x]--;
+            }
+            ops++;
+            if(dup == 0) return ops;
+        }
+        return ops;
     }
 };
