804a4de781505b919db8b12177f3b32de97fe630
Uzer447 <103814661+Uzer447@users.noreply.github.com>
2025-04-25 12:58:12 +0530

    Time: 48 ms (78.14%) | Memory: 129 MB (6.56%) - LeetSync

---
+++ 2915-count-of-interesting-subarrays/count-of-interesting-subarrays.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    long long countInterestingSubarrays(vector<int>& nums, int modulo, int k) {
+        unordered_map<long long,long long> cnt;
+        cnt.reserve(nums.size()*2);
+        cnt.max_load_factor(0.7);
+        cnt[0]=1;
+        long long pref=0, ans=0;
+        for(int x:nums){
+            if(x%modulo==k) ++pref;
+            long long cur=pref%modulo;
+            long long need=(cur-k)%modulo;
+            if(need<0) need+=modulo;
+            auto it=cnt.find(need);
+            if(it!=cnt.end()) ans+=it->second;
+            ++cnt[cur];
+        }
+        return ans;
+    }
+};
