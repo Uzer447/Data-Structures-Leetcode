2d712303b7e824dc5f2f243489bcb87e1a0dec1b
Uzer447 <103814661+Uzer447@users.noreply.github.com>
2025-05-07 15:28:44 +0530

    Time: 23 ms (52.35%), Space: 30.3 MB (36.91%) - LeetHub

--- 3627-find-minimum-time-to-reach-last-room-i/3627-find-minimum-time-to-reach-last-room-i.cpp
+++ 3627-find-minimum-time-to-reach-last-room-i/3627-find-minimum-time-to-reach-last-room-i.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-    int minTimeToReach(vector<vector<int>>& moveTime) {
-        int n = moveTime.size(), m = moveTime[0].size();
-        const long long INF = 4e18;
-        vector<vector<long long>> dist(n, vector<long long>(m, INF));
-        using State = tuple<long long,int,int>;
-        priority_queue<State, vector<State>, greater<State>> pq;
-        dist[0][0] = 0;
+    int minTimeToReach(vector<vector<int>>& g) {
+        int n=g.size(),m=g[0].size();
+        const long long INF=4e18;
+        vector<vector<long long>> d(n,vector<long long>(m,INF));
+        using T=tuple<long long,int,int>;
+        priority_queue<T,vector<T>,greater<T>> pq;
+        d[0][0]=0;
         pq.emplace(0,0,0);
-        int dr[4]={1,-1,0,0}, dc[4]={0,0,1,-1};
+        int dr[4]={1,-1,0,0},dc[4]={0,0,1,-1};
         while(!pq.empty()){
-            auto [t,r,c]=pq.top(); pq.pop();
-            if(t!=dist[r][c]) continue;
-            if(r==n-1 && c==m-1) return t;
+            auto[t,r,c]=pq.top();pq.pop();
+            if(t!=d[r][c])continue;
+            if(r==n-1&&c==m-1)return t;
             for(int k=0;k<4;k++){
-                int nr=r+dr[k], nc=c+dc[k];
-                if(nr<0||nr>=n||nc<0||nc>=m) continue;
-                long long depart=max<long long>(t,moveTime[nr][nc]);
-                long long arr=depart+1;
-                if(arr<dist[nr][nc]){
-                    dist[nr][nc]=arr;
-                    pq.emplace(arr,nr,nc);
+                int nr=r+dr[k],nc=c+dc[k];
+                if(nr<0||nr>=n||nc<0||nc>=m)continue;
+                long long nt=max<long long>(t,g[nr][nc])+1;
+                if(nt<d[nr][nc]){
+                    d[nr][nc]=nt;
+                    pq.emplace(nt,nr,nc);
                 }
             }
         }
