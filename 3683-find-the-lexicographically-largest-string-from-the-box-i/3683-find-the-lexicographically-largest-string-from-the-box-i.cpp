2bcdf7624f18bb103b2cea5a91d38d5e1c336048
Uzer447 <103814661+Uzer447@users.noreply.github.com>
2025-06-04 12:24:44 +0530

    Time: 12 ms (88.81%), Space: 37 MB (76.49%) - LeetHub

--- 3683-find-the-lexicographically-largest-string-from-the-box-i/3683-find-the-lexicographically-largest-string-from-the-box-i.cpp
+++ 3683-find-the-lexicographically-largest-string-from-the-box-i/3683-find-the-lexicographically-largest-string-from-the-box-i.cpp
@@ -2,35 +2,21 @@ class Solution {
 public:
     string answerString(string word, int numFriends) {
         int n = word.length();
-        if (numFriends == 1) {
-            // Only one friend, the entire word is collected
-            return word;
-        } else if (numFriends == n) {
-            // Each friend gets one character; return the lex largest character
+        if (numFriends == 1) return word;
+        if (numFriends == n) {
             char max_char = *max_element(word.begin(), word.end());
             return string(1, max_char);
-        } else {
-            int L = n - numFriends + 1; // Maximum possible length of a part
-            // Find the lexicographically largest character in the word
-            char max_char = *max_element(word.begin(), word.end());
-            string max_substring = "";
-            // Positions where the character is equal to the max_char
-            vector<int> positions;
-            for (int i = 0; i < n; ++i) {
-                if (word[i] == max_char) {
-                    positions.push_back(i);
-                }
-            }
-            // Consider substrings starting at positions of max_char with length
-            // up to L
-            for (int i : positions) {
-                int end = min(i + L, n);
-                string s = word.substr(i, end - i);
-                if (s > max_substring) {
-                    max_substring = s;
-                }
-            }
-            return max_substring;
         }
+        int L = n - numFriends + 1;
+        char max_char = *max_element(word.begin(), word.end());
+        string max_substring = "";
+        vector<int> positions;
+        for (int i = 0; i < n; ++i) if (word[i] == max_char) positions.push_back(i);
+        for (int i : positions) {
+            int end = min(i + L, n);
+            string s = word.substr(i, end - i);
+            if (s > max_substring) max_substring = s;
+        }
+        return max_substring;
     }
 };
