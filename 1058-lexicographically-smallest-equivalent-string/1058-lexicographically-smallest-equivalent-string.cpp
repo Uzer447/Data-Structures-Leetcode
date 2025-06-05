2fea345101e121b62934c0324460af6597b56e89
Uzer447 <103814661+Uzer447@users.noreply.github.com>
2025-06-05 12:00:22 +0530

    Time: 3 ms (41.19%), Space: 8.9 MB (47.31%) - LeetHub

--- 1058-lexicographically-smallest-equivalent-string/1058-lexicographically-smallest-equivalent-string.cpp
+++ 1058-lexicographically-smallest-equivalent-string/1058-lexicographically-smallest-equivalent-string.cpp
@@ -1,23 +1,18 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
+    int par[26];
+    int find(int x){ return par[x]==x?x:par[x]=find(par[x]); }
+    void unite(int a,int b){
+        a=find(a); b=find(b);
+        if(a==b) return;
+        if(a<b) par[b]=a;
+        else par[a]=b;
+    }
 public:
-    string smallestEquivalentString(string s1, string s2, string baseStr) {
-        vector<int> p(26);
-        iota(p.begin(), p.end(), 0);
-        function<int(int)> find = [&](int x) { return p[x] == x ? x : p[x] = find(p[x]); };
-        auto unite = [&](int a, int b) {
-            a = find(a);
-            b = find(b);
-            if (a == b) return;
-            if (a < b) p[b] = a;
-            else p[a] = b;
-        };
-        for (int i = 0; i < s1.size(); ++i) unite(s1[i] - 'a', s2[i] - 'a');
-        string res;
-        res.reserve(baseStr.size());
-        for (char c : baseStr) res.push_back(char(find(c - 'a') + 'a'));
-        return res;
+    string smallestEquivalentString(string s1,string s2,string baseStr){
+        iota(begin(par),end(par),0);
+        for(int i=0;i<s1.size();++i) unite(s1[i]-'a',s2[i]-'a');
+        string ans; ans.reserve(baseStr.size());
+        for(char c:baseStr) ans.push_back(char(find(c-'a')+'a'));
+        return ans;
     }
 };
