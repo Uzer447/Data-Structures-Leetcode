d41aeaf116262ea65ec4b3a53d66d71070826c81
Uzer447 <103814661+Uzer447@users.noreply.github.com>
2025-07-13 10:43:57 +0530

    Time: 54 ms (20.19%) | Memory: 52.9 MB (15.15%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,18 +1,55 @@
 class Solution {
 public:
-    int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount+1,1e9);
-        dp[0]=0;
-        for(int amt=1;amt<=amount;amt++)
+    int func(int ind,int n,int amt,vector<int> &coins)
+    {
+        if(ind==n-1)
         {
-            for(int i=0;i<coins.size();i++)
+            if(amt%coins[ind]==0)
             {
-                if(amt-coins[i]>=0)
-                dp[amt]=min(dp[amt],1+dp[amt-coins[i]]);
+                return amt/coins[ind];
+            }
+            else
+                return 1e9;
+        }
+        int take=1e9;
+        int nottake=func(ind+1,n,amt,coins);
+        if(amt>=coins[ind])
+        {
+            take=1+func(ind,n,amt-coins[ind],coins);
+        }
+        return min(take,nottake);
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        int n=coins.size();
+        vector<vector<int>> dp(n+1,vector<int>(amount+1,0));
+        for(int i=0;i<=amount;i++)
+        {
+            if(i%coins[n-1]==0)
+            {
+                dp[n-1][i]=i/coins[n-1];
+            }
+            else
+                dp[n-1][i]=1e9;
+        }
+        for(int i=n-2;i>=0;i--)
+        {
+            for(int amt=0;amt<=amount;amt++)
+            {
+                int take=1e9;
+                int nottake=dp[i+1][amt];
+                if(amt>=coins[i])
+                {
+                    take=1+dp[i][amt-coins[i]];
+                }
+                dp[i][amt]=min(take,nottake);
             }
         }
-        if(dp[amount]==1e9)
-            dp[amount]=-1;
-        return dp[amount];
+        if(dp[0][amount]==1e9) return -1;
+        return dp[0][amount];
+        int ans=func(0,n,amount,coins);
+        if(ans==1e9)
+            return -1;
+        return ans;
+
     }
 };
