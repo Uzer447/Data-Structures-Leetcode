a301275778ad344c127038aad669952796be0b61
Uzer447 <103814661+Uzer447@users.noreply.github.com>
2025-07-14 11:15:16 +0530

    Time: 6 ms (66.33%), Space: 13.1 MB (45.93%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -24,7 +24,7 @@ public:
                 }
                 else
                 {
-                    dp[i][j]=1+min(dp[i-1][j],min(dp[i][j-1],dp[i-1][j-1]));
+                    dp[i][j]=1+min(dp[i-1][j-1],min(dp[i-1][j],dp[i][j-1]));
                 }
             }
         }
