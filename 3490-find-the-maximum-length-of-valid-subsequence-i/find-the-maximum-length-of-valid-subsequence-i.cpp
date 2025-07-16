bb6e5c9f8210a1142b745626fd6d7c87cc3b7232
Uzer447 <103814661+Uzer447@users.noreply.github.com>
2025-07-16 11:26:29 +0530

    Time: 0 ms (100.00%) | Memory: 96.3 MB (40.00%) - LeetSync

---
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int maximumLength(std::vector<int>& nums) {
+        int even_count = 0;
+        int odd_count = 0;
+        int alt_eo = 0;
+        int alt_oe = 0;
+        int expected_eo_parity = 0;
+        int expected_oe_parity = 1;
+
+        for (int x : nums) {
+            int current_parity = x % 2;
+
+            if (current_parity == 0) {
+                even_count++;
+            } else {
+                odd_count++;
+            }
+
+            if (current_parity == expected_eo_parity) {
+                alt_eo++;
+                expected_eo_parity = 1 - expected_eo_parity;
+            }
+
+            if (current_parity == expected_oe_parity) {
+                alt_oe++;
+                expected_oe_parity = 1 - expected_oe_parity;
+            }
+        }
+
+        return std::max({even_count, odd_count, alt_eo, alt_oe});
+    }
+};
