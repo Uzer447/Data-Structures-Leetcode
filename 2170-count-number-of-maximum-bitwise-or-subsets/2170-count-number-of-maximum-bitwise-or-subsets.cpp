b1e2bea81ec6fe36c7df91c7bda1faf295a01e42
Uzer447 <103814661+Uzer447@users.noreply.github.com>
2025-07-28 22:44:06 +0530

    Time: 9 ms (74.52%), Space: 18.2 MB (22.95%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     int countMaxOrSubsets(vector<int>& nums) {
-        int max = 0;
-        vector<int> dp(1 << 17, 0);
-        dp[0] = 1;
-        for (int num : nums) {
-            for (int i = max; i >= 0; i--) {
-                dp[i | num] += dp[i];
-            }
-            max |= num;
+        int n = nums.size();
+        int max_or = 0;
+        for (int x : nums) max_or |= x;
+        int m = 1 << n;
+        vector<int> dp(m);
+        int ans = 0;
+        for (int mask = 1; mask < m; ++mask) {
+            int b = __builtin_ctz(mask);
+            dp[mask] = dp[mask ^ (1 << b)] | nums[b];
+            if (dp[mask] == max_or) ++ans;
         }
-
-        return dp[max];
+        return ans;
     }
 };
