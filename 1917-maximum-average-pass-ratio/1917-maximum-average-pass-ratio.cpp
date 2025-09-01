9d43cc4ba867a97f9dbb7e84e75bf279c9cb5d36
Uzer447 <103814661+Uzer447@users.noreply.github.com>
2025-09-01 14:36:02 +0530

    Time: 514 ms (13.16%), Space: 103.3 MB (26.61%) - LeetHub

--- 1917-maximum-average-pass-ratio/1917-maximum-average-pass-ratio.cpp
+++ 1917-maximum-average-pass-ratio/1917-maximum-average-pass-ratio.cpp
@@ -1,37 +1,27 @@
 class Solution {
+    struct Node {
+        long long p, t;
+        double g;
+        bool operator<(const Node& o) const { return g < o.g; }
+    };
+    static double gain(long long p, long long t) {
+        return (double)(t - p) / (t * (t + 1.0));
+    }
 public:
     double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {
-        // Lambda to calculate the gain of adding an extra student
-        auto calculateGain = [](int passes, int totalStudents) {
-            return (double)(passes + 1) / (totalStudents + 1) -
-                   (double)passes / totalStudents;
-        };
-
-        // Max heap to store (-gain, passes, totalStudents)
-        priority_queue<pair<double, pair<int, int>>> maxHeap;
-        for (const auto& singleClass : classes) {
-            maxHeap.push({calculateGain(singleClass[0], singleClass[1]),
-                          {singleClass[0], singleClass[1]}});
-        }
-
-        // Distribute extra students
+        priority_queue<Node> pq;
+        for (auto& v : classes) pq.push({v[0], v[1], gain(v[0], v[1])});
         while (extraStudents--) {
-            auto [currentGain, classInfo] = maxHeap.top();
-            maxHeap.pop();
-            int passes = classInfo.first;
-            int totalStudents = classInfo.second;
-            maxHeap.push({calculateGain(passes + 1, totalStudents + 1),
-                          {passes + 1, totalStudents + 1}});
+            auto cur = pq.top(); pq.pop();
+            ++cur.p; ++cur.t;
+            cur.g = gain(cur.p, cur.t);
+            pq.push(cur);
         }
-
-        // Calculate the final average pass ratio
-        double totalPassRatio = 0;
-        while (!maxHeap.empty()) {
-            auto [_, classInfo] = maxHeap.top();
-            maxHeap.pop();
-            totalPassRatio += (double)classInfo.first / classInfo.second;
+        long double s = 0;
+        for (; !pq.empty(); pq.pop()) {
+            auto cur = pq.top();
+            s += (long double)cur.p / cur.t;
         }
-
-        return totalPassRatio / classes.size();
+        return (double)(s / classes.size());
     }
 };
