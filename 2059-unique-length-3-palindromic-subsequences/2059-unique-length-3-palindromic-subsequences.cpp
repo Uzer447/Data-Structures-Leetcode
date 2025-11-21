c3c26585aca26810f943ff8574d21a911d6edaa8
Uzer447 <103814661+Uzer447@users.noreply.github.com>
2025-11-21 23:09:22 +0530

    Time: 194 ms (28.09%), Space: 16 MB (43.15%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.cpp
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.cpp
@@ -1,29 +1,35 @@
 class Solution {
 public:
     int countPalindromicSubsequence(string s) {
-        int N = s.size();
-        vector<int> pre(N), suf(N);
-        pre[0] = 1 << (s[0] - 'a');
-        for (int i = 1; i < N; ++i)
-            pre[i] = pre[i - 1] | (1 << (s[i] - 'a'));
-        suf[N - 1] = 1 << (s[N - 1] - 'a');
-        for (int i = N - 2; i >= 0; --i)
-            suf[i] = suf[i + 1] | (1 << (s[i] - 'a'));
-        bool pal[26][26] = {};
-        for (int k = 1; k < N - 1; ++k) {
-            int x = s[k] - 'a';
-            int before = pre[k - 1];
-            int after = suf[k + 1];
-            for (int c = 0; c < 26; ++c) {
-                if ((before & (1 << c)) && (after & (1 << c))) {
-                    pal[c][x] = true;
+        unordered_set<char> letters;
+        for (char c : s) {
+            letters.insert(c);
+        }
+        
+        int ans = 0;
+        for (char letter : letters) {
+            int i = -1;
+            int j = 0;
+            
+            for (int k = 0; k < s.size(); k++) {
+                if (s[k] == letter) {
+                    if (i == -1) {
+                        i = k;
+                    }
+                    
+                    j = k;
                 }
             }
+            
+            unordered_set<char> between;
+            for (int k = i + 1; k < j; k++) {
+                between.insert(s[k]);
+            }
+            
+            ans += between.size();
         }
-        int count = 0;
-        for (int c = 0; c < 26; ++c)
-            for (int x = 0; x < 26; ++x)
-                if (pal[c][x]) ++count;
-        return count;
+        
+        return ans;
     }
 };
+
