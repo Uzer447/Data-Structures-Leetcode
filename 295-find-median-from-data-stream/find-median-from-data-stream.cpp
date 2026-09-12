e1ae52c68ea6383b78d4fb6498b2642d07344dcc
Uzer447 <103814661+Uzer447@users.noreply.github.com>
2026-09-12 19:23:30 +0530

    Time: 69 ms (63.92%) | Memory: 148.7 MB (8.89%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.cpp
+++ 295-find-median-from-data-stream/find-median-from-data-stream.cpp
@@ -1,41 +1,35 @@
 class MedianFinder {
-    priority_queue<int> left;
-    priority_queue<int,vector<int>,greater<int>> right;
+    priority_queue<int> left; //max heap
+    priority_queue<int,vector<int>,greater<int>> right; //min heap
 public:
-
     MedianFinder() {
         
     }
     
     void addNum(int num) {
         left.push(num);
-        if(!right.empty() && left.top()>right.top())
-        {
+        if(left.size()>right.size()+1){
             right.push(left.top());
             left.pop();
         }
-        if(left.size()>right.size()+1)
-        {
+        if(!right.empty() && left.top()>right.top()){
             right.push(left.top());
             left.pop();
         }
-        if(right.size()>left.size()+1)
-        {
+        if(right.size()>left.size()+1){
             left.push(right.top());
             right.pop();
         }
     }
     
     double findMedian() {
-        if(left.size()==right.size())
-        {
+        if(left.size()==right.size()){
             return (left.top()+right.top())/2.0;
         }
-        if(left.size()>right.size())
+        if(left.size()>right.size()){
             return left.top();
-        if(right.size()>left.size())
-            return right.top();
-        return 0;
+        }
+        return right.top();
     }
 };
 
