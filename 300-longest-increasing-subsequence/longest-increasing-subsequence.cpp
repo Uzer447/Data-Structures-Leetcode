36dbd95e1bda74d3d02d1935e086ee8ea9ec04b3
Uzer447 <103814661+Uzer447@users.noreply.github.com>
2026-09-14 23:09:14 +0530

    Time: 63 ms (61.56%) | Memory: 14.2 MB (71.07%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,33 +1,16 @@
 class Solution {
 public:
-    int func(int ind,int n,int lastele,vector<int> &nums)
-    {
-        if(ind>=n)
-            return 0;
-        int take=0;
-        int nottake=func(ind+1,n,lastele,nums);
-        if(lastele==-1 || nums[ind]>nums[lastele])
-        {
-            take=1+func(ind+1,n,ind,nums);
-        }
-        return max(take,nottake);
-    }
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
-        for(int i=n-1;i>=0;i--)
-        {
-            for(int prevind=-1;prevind<=i-1;prevind++)
-            {
-                int take=0;
-                int nottake=dp[i+1][prevind+1];
-                if(prevind==-1 || nums[i]>nums[prevind])
-                {
-                    take=1+dp[i+1][i+1];
+        vector<int> dp(n,1);
+        dp[0]=1;
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[j]<nums[i]){
+                    dp[i]=max(dp[i],dp[j]+1);
                 }
-                dp[i][prevind+1]=max(take,nottake);
             }
         }
-        return dp[0][0];
+        return *max_element(dp.begin(),dp.end());
     }
 };
