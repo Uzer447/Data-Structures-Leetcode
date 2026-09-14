e029b3ff596a02bafa5ee116b17e167c1b971599
Uzer447 <103814661+Uzer447@users.noreply.github.com>
2026-09-14 13:58:28 +0530

    Time: 95 ms (34.48%) | Memory: 21.4 MB (40.87%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -4,26 +4,21 @@ public:
         queue<pair<string,int>> q;
         q.push({beginWord,1});
         unordered_set<string> st(wordList.begin(),wordList.end());
-        st.erase(beginWord);
-        while(!q.empty())
-        {
+        while(!q.empty()){
             string word=q.front().first;
             int steps=q.front().second;
             q.pop();
             if(word==endWord) return steps;
-            for(int i=0;i<word.size();i++)
-            {
-                char original=word[i];
-                for(char ch='a'; ch<='z';ch++)
-                {
-                    word[i]=ch;
-                    if(st.find(word)!=st.end())
-                    {
-                        q.push({word,steps+1});
-                        st.erase(word);
+            for(int i=0;i<word.size();i++){
+                for(int j='a';j<='z';j++){
+                    string newWord=word;
+                    newWord[i]=j;
+                    if(newWord==word) continue;
+                    if(st.count(newWord)==1){
+                        q.push({newWord,steps+1});
+                        st.erase(newWord);
                     }
                 }
-                word[i]=original;
             }
         }
         return 0;
