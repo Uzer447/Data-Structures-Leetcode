3be54b3d1c616cfcdd3754d86bdb904345f6d2eb
Uzer447 <103814661+Uzer447@users.noreply.github.com>
2026-09-14 19:17:39 +0530

    Time: 4 ms (47.73%) | Memory: 18.5 MB (28.88%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,37 +1,35 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<pair<int,int>> adj[n];
-        for(auto it:flights)
-        {
+        vector<vector<pair<int,int>>> adj(n);
+        for(auto it:flights){
             adj[it[0]].push_back({it[1],it[2]});
         }
         queue<pair<int,pair<int,int>>> q;
         q.push({0,{src,0}});
-        vector<int> dist(n,1e9);
+        vector<int> dist(n,1e8);
+        int ans=INT_MAX;
         dist[src]=0;
-        while(!q.empty())
-        {
-            auto it=q.front();
+        while(!q.empty()){
+            int stops=q.front().first;
+            int node=q.front().second.first;
+            int distance=q.front().second.second;
             q.pop();
-            int stops=it.first;
-            int node=it.second.first;
-            int cost=it.second.second;
-            if(stops>k)
-            continue;
-            for(auto it:adj[node])
-            {
-                int adjNode=it.first;
-                int edW=it.second;
-                if(cost+edW<dist[adjNode])
-                {
-                    dist[adjNode]=cost+edW;
-                    q.push({stops+1,{adjNode,dist[adjNode]}});
+            if(stops>k){
+                if(node==dst){
+                    ans=min(ans,distance);
+                } else{
+                    continue;
+                }
+            }
+            for(auto it:adj[node]){
+                if(distance + it.second < dist[it.first]){
+                    dist[it.first]=distance+it.second;
+                    q.push({stops+1,{it.first,distance+it.second}});
                 }
             }
         }
-        if(dist[dst]==1e9)
-        return -1;
+        if(dist[dst]==1e8) return -1;
         return dist[dst];
     }
 };
